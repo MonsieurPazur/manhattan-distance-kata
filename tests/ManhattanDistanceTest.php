c9508f7726f7e5f050775ccdf1af96fa938b7393
Michał Skwarek <michal.skwarek@hotmail.com>
2019-05-20 09:24:07 +0200

    Initial commit, empty test class

---
+++ tests/ManhattanDistanceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Basic test suite for calculating manhattan distance.
+ */
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ManhattanDistanceTest
+ * @package Test
+ */
+class ManhattanDistanceTest extends TestCase
+{
+
+}
